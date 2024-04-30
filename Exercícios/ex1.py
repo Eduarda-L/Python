a025a0c03362673e8e60bf2a84cb347da2034262
Eduarda-L <Izaeduarda33@gmail.com>
2024-04-29 21:45:56 -0300

    Calculadora
    Calculadora em py

---
+++ Exercícios/ex1.py
@@ -0,0 +1,24 @@
+#exercícios1
+
+nome = input("Digite seu nome: ")
+print("Olá {}, seja bem vindo!".format(nome))
+
+#exercícios2
+
+n1 = input("Digite um valor: ")
+n2 = input("Digite outro valor: ")
+
+soma = int(n1) + int(n2)
+
+print("A soma dos valores é: {}".format(soma))
+
+#exercícios3
+
+n = input("Digite um número: ")
+a = n - 1
+s = n + 1
+print("Analisando o valor {}, seu antecessor é {}".format(n, a))
+
+
+
+#\n para quebrar a linha
--- Variáveis/aula1.py
+++ Variáveis/aula1.py
@@ -1,1 +1,17 @@
-print("Hello World")
+#Teste de Variáveis
+
+n1 = 1.5
+n2 = 3.0
+res = n1 + n2
+print(res)
+
+#Controle de Fluxo
+
+valor1 = 10
+valor2 = 20
+
+if valor1 > valor2:
+    print("O valor1 é maior do que o valor2")
+          
+else:
+    print("o valor2 é maior do que o valor1")
