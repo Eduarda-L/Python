3c1cb291f51e2922873997ed18f51c2be00d2289
Eduarda-L <Izaeduarda33@gmail.com>
2024-05-01 12:06:50 -0300

    ex7.py

---
+++ Exercícios/ex7.py
@@ -0,0 +1,8 @@
+#Reajuste Salarial
+
+salario = float(input("informe seu salario: "))
+aumento = salario * 0.15
+
+salario_final = salario + aumento
+
+print("Seu salario com aumento é de: ", salario_final)
