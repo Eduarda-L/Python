e4a071e67261bbef01a535d626f4144211e209da
Eduarda-L <Izaeduarda33@gmail.com>
2024-05-01 21:27:06 -0300

    ex9.py

---
+++ Exercícios/ex9.py
@@ -0,0 +1,6 @@
+n1 = float(input("quantos dias alugados? "))
+n2 = float(input("quantos km rodados? ")) 
+dia = 20
+km = 10
+res = dia * n1 + km * n2
+print("o total a pagar é de R${:.2f}".format(res))
